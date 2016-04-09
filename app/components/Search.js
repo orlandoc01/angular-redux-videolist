e6c09cac3b5688f2bc83b2fce5380f3d6f7d30d0
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-04-08 20:10:53 -0700

    Connect Search to Redux

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,4 +1,25 @@
+import {setVideoList, setPlayingVideo} from '../actions/actions.js';
+import searchYoutube from '../utils/utils.js';
+
 class SearchCtrl {
+  constructor($ngRedux) {
+    $ngRedux.connect(this.mapStateToThis, this.mapDispatchToThis)(this);
+  }
+  mapStateToThis(state) {
+    return {};
+  }
+  mapDispatchToThis(dispatch) {
+    return {
+      searchText: (e) => {
+        searchYoutube({q: e.target.value})
+        .then(q => {
+          dispatch(setVideoList(q.items));
+          dispatch(setPlayingVideo(q.items[0]));
+        })
+        .catch(console.error);
+      }
+    };
+  }
 }
 
 const createSearchCtrl = () => {
